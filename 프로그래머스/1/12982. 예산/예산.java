c265fadabfe7ce37f173e325a8211e4e557210c7
황세희 <36257239+sehee210@users.noreply.github.com>
2024-06-13 19:24:21 +0900

    [level 1] Title: 예산, Time: 0.48 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        int sum = 0;
+        
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if (sum <= budget) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
